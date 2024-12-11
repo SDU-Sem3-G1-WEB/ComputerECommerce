79565fff4b368a9193e98fa8b3663f642a548cea
Zuhao <yalin1998@hotmail.com>
2024-12-11 19:22:11 +0100

    Added Danish localization to index

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
 using ComputerECommerce.Data;
 using Microsoft.EntityFrameworkCore;
-
-using ComputerECommerce.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
 builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<DataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("MainDb")));
 
 builder.Services.AddDbContext<DataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("MainDb")));
 
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+// Localization
+builder.Services.Configure<RequestLocalizationOptions>(options => {
+    var supportedCultures = new List<CultureInfo>
+    {
+        new CultureInfo("en"),
+        new CultureInfo("da")
+    };
+    options.DefaultRequestCulture = new RequestCulture("en");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
 var app = builder.Build();
+app.UseRequestLocalization();
 
 // Apply migrations and seed the database automatically
 using (var scope = app.Services.CreateScope())
